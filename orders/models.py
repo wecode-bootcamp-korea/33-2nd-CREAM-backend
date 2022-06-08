0f315557c6c4d6a9ee3b5f791b9de42127c447d4
strzero <restouri@gmail.com>
2022-06-08 14:36:03 +0900

    models migrated

---
+++ orders/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+from core.models     import TimeStampedModel
+from users.models    import User
+from products.models import ProductSize
+
+class BidType(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'bid_types'
+
+    def __str__(self):
+        return self.name
+
+class Bid(TimeStampedModel):
+    user         = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids')
+    type         = models.ForeignKey(BidType, on_delete=models.CASCADE, related_name='bids')
+    product_size = models.ForeignKey(ProductSize, on_delete=models.CASCADE, related_name='bids')
+    price        = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'bids'
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'order_status'
+    
+    def __str__(self):
+        return self.name
+
+class Order(TimeStampedModel):
+    seller       = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sell_orders')
+    buyer        = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buy_orders')
+    bid          = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name='orders')
+    price        = models.DecimalField(max_digits=10, decimal_places=2)
+    product_size = models.ForeignKey(ProductSize, on_delete=models.SET_NULL, null=True, related_name='orders')
+    status       = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, related_name='orders')
+
+    class Meta:
+        db_table = 'orders'
+
---
+++ products/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+from core.models import TimeStampedModel
+
+class ProductCateogory(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'product_categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(TimeStampedModel):
+    english_name  = models.CharField(max_length=100)
+    korean_name   = models.CharField(max_length=100)
+    category      = models.ForeignKey(ProductCateogory, on_delete=models.CASCADE, related_name='products')
+    model_number  = models.CharField(max_length=50)
+    release_date  = models.DateField()
+    release_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return self.korean_name
+    
+class ProductImage(models.Model):
+    product   = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField(max_length=600)
+
+    class Meta:
+        db_table = 'product_images'
+
+    def __str__(self):
+        return self.image_url
+
+class Size(models.Model):
+    name    = models.CharField(max_length=20)
+    product = models.ManyToManyField(Product, through='productsize' , related_name='sizes')
+
+    class Meta:
+        db_table = 'sizes'
+
+    def __str__(self):
+        return self.name
+
+class ProductSize(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size    = models.ForeignKey(Size, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product_sizes'
