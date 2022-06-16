f56b6693d146df05f1f1a8233ff38361b4974247
strzero <restouri@gmail.com>
2022-06-16 09:45:12 +0900

    입찰하기, 주문하기 기능구현

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import OrderListView
+from .views import OrderListView, BidView, OrderView
 
 urlpatterns = [
     path('/<int:product_id>/orders', OrderListView.as_view()),
+    path('/<int:product_id>/bids', BidView.as_view()),
+    path('/<int:product_id>/order', OrderView.as_view()),
 ]
