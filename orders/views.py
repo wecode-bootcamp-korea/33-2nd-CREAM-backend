1818c32eb73a61cd7b7268d679455045a91de6b5
strzero <restouri@naver.com>
2022-06-20 10:13:17 +0900

    bids app add

--- orders/views.py
+++ orders/views.py
@@ -34,6 +34,7 @@ class BidView(View):
         if not products.exists():
             return JsonResponse({'message': 'PRODUCT_SIZE_NOT_EXIST'}, status=404)
 
+        #리팩토링
         if bid_type == 'buy':
             products = products\
                 .annotate(price=Min("bids__price", filter=Q(bids__type_id=BidTypeEnum.SELL.value)))
@@ -60,6 +61,7 @@ class BidView(View):
             size_name = data['size_name']
             bid_price = data['price']
 
+            #리팩토링
             bid_type_set = {
                 'buy' : BidTypeEnum.BUY.value,
                 'sell': BidTypeEnum.SELL.value
@@ -97,16 +99,16 @@ class OrderView(View):
             data       = json.loads(request.body)
             size_name  = data['size_name']
 
-            if (order_type != 'buy') and (order_type != 'sell'):
-                return JsonResponse({"message": "ORDER_TYPE_ERROR"}, status=400)
-
-            product_size_id = ProductSize.objects.get(size__name=size_name, product_id=product_id).id
-
             bid_type_id_set = {
                 'buy' : BidTypeEnum.SELL.value,
                 'sell': BidTypeEnum.BUY.value
             }
             bid_type_id = bid_type_id_set.get(order_type)
+
+            if not bid_type_id:
+                return JsonResponse({"message": "ORDER_TYPE_ERROR"}, status=400)
+
+            product_size_id = ProductSize.objects.get(size__name=size_name, product_id=product_id).id
 
             product = Product.objects.annotate(
                 buy_now_price = Min("productsize__bids__price",
@@ -122,13 +124,16 @@ class OrderView(View):
             price = product.buy_now_price if order_type == 'buy' else product.sell_now_price
 
             bid = Bid.objects.filter(
-                product_size_id=product_size_id, type_id=bid_type_id, price=price).order_by('created_at')
-            if not bid.exists():
+                product_size_id=product_size_id, type_id=bid_type_id, price=price).order_by('created_at').first()
+
+            if not bid:
                 return JsonResponse({"message": "BID_NOT_EXIST"}, status=404)
-            bid = bid.first()
 
             buyer  = request.user if order_type == 'buy' else bid.user
             seller = request.user if order_type == 'sell' else bid.user
+
+            if buyer == seller:
+                return JsonResponse({"message": "CANT_ORDER_YOUR_OWN_BID"}, status=400)
 
             if buyer.point < price:
                 return JsonResponse({"message": "NOT_ENOUGH_POINT"}, status=400)
@@ -165,4 +170,4 @@ class OrderView(View):
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
         except transaction.TransactionManagementError:
-            return JsonResponse({'message': 'TRANSACTION_MANAGEMENT_ERROR'}, status=401)          
+            return JsonResponse({'message': 'TRANSACTION_MANAGEMENT_ERROR'}, status=401)       
